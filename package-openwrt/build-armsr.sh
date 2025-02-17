bccd783734b514bacc7895948f928157b80e7d70
aday-allbase <maxroepz48@gmail.com>
2025-02-17 21:36:26 +0700

    aday

--- package-openwrt/build-armsr.sh
+++ package-openwrt/build-armsr.sh
@@ -1,63 +1,47 @@
 #!/bin/bash
 
-# Print OpenWrt build info
+# Profile info
 make info
 
-# Set profile for armsr
+# Main configuration name
 PROFILE="generic"
 
 PACKAGES=""
 
-# Selecting default packages
-    PACKAGES="$PACKAGES cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua \
-    zram-swap adb parted losetup resize2fs luci-ssl block-mount htop wget wget-ssl \
-    tar unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server \
-    liblucihttp liblucihttp-lua libubus-lua lua luci-app-firewall luci-app-opkg \
-    luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
-    luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp \
-    luci-theme-bootstrap px5g-wolfssl rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci \
-    rpcd-mod-rrdns uhttpd uhttpd-mod-ubus usbutils \
-    kmod-usb-net kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-acm kmod-usb-net-qmi-wwan \
-    kmod-usb-net-rndis kmod-usb-serial-qualcomm kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial \
-    kmod-nls-utf8 kmod-usb-serial-option kmod-usb-serial-sierrawireless kmod-usb-uhci kmod-usb2 \
-    kmod-usb-net-ipheth kmod-usb-net-cdc-mbim usbmuxd libusbmuxd-utils libimobiledevice-utils \
-    mbim-utils qmi-utils uqmi umbim modemmanager modemmanager-rpcd luci-proto-modemmanager libmbim libqmi luci-proto-3g luci-proto-ncm \
-    usb-modeswitch picocom minicom libusb-1.0-0 \
-    kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
-    ca-bundle coreutils-sleep fontconfig coreutils-whoami file lolcat \
-    zsh kmod-mii kmod-usb-wdm kmod-usb-serial-wwan kmod-usb-ehci kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3 iptables-nft coreutils-stty \
-    kmod-usb-storage kmod-usb-storage-uas ntfs-3g iw-full \
-    libqrtr-glib luci-proto-qmi \
-    perl perlbase-base perlbase-bytes perlbase-class perlbase-config perlbase-cwd perlbase-dynaloader perlbase-errno perlbase-essential perlbase-fcntl perlbase-file \
-    perlbase-filehandle perlbase-i18n perlbase-integer perlbase-io perlbase-list perlbase-locale perlbase-params perlbase-posix \
-    perlbase-re perlbase-scalar perlbase-selectsaver perlbase-socket perlbase-symbol perlbase-tie perlbase-time perlbase-unicore perlbase-utf8 perlbase-xsloader \
-    ruby-bigdecimal ruby-date ruby-digest ruby-enc ruby-forwardable ruby-pstore ruby-psych ruby-stringio \
-    luci-lua-runtime zoneinfo-asia zoneinfo-core luci-proto-mbim \
-    libc coreutils-stat libopenssl-legacy \
-    sms-tool cpusage ttyd dmesg luci-lib-ipkg \
-    iptables iptables-legacy iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat \
-    coreutils coreutils-base64 libuci-lua"
-    
-    # Openclash
-    PACKAGES="$PACKAGES coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base"
-    
-    # Material Theme
-    PACKAGES="$PACKAGES luci-theme-material"
-    
-    # PHP8
-    PACKAGES="$PACKAGES php8 php8-cgi php8-fastcgi php8-fpm php8-mod-ctype php8-mod-curl php8-mod-fileinfo php8-mod-iconv php8-mod-mbstring php8-mod-session php8-mod-zip \
-    php8-cli php8-mod-bcmath php8-mod-calendar php8-mod-filter php8-mod-gd php8-mod-intl \
-    php8-mod-mysqli php8-mod-mysqlnd php8-mod-opcache php8-mod-pdo php8-mod-pdo-mysql php8-mod-phar \
-    php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter"
-    
-    # No
-    PACKAGES="$PACKAGES -procd-ujail -libgd -dnsmasq"
-    
-    # amlogic
-    PACKAGES="$PACKAGES ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant"
-    
-# Files directory for custom configurations or files
+# Modem and UsbLAN Driver
+PACKAGES=" kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179"
+PACKAGES=" kmod-mii kmod-usb-net kmod-usb-wdm kmod-usb-net-qmi-wwan uqmi luci-proto-qmi \
+kmod-usb-net-cdc-ether kmod-usb-serial-option kmod-usb-serial kmod-usb-serial-wwan qmi-utils \
+kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim umbim \
+modemmanager luci-proto-modemmanager libmbim libqmi usbutils luci-proto-mbim luci-proto-ncm \
+kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial-sierrawireless \
+kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-utils libimobiledevice-utils usb-modeswitch kmod-nls-utf8 mbim-utils xmm-modem \
+kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3"
+
+# Tunnel option
+PACKAGES=" coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base"
+
+# NAS and Hard disk tools
+PACKAGES=" kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
+
+# Speedtest
+PACKAGES=" iperf3"
+
+# Material Theme
+PACKAGES=" luci-theme-material"
+
+# PHP8
+PACKAGES=" libc php8 php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring coreutils-stat zoneinfo-asia"
+
+# Amlogic
+    PACKAGES=" ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant"
+    EXCLUDED=" -procd-ujail -dnsmasq"
+
+# Misc
+    PACKAGES=" zram-swap adb parted losetup resize2fs luci luci-ssl block-mount htop bash curl wget wget-ssl tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server"
+
+# Custom Files
 FILES="files"
 
-# Generate the image with specified profile and packages
-make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES"
+# Start build firmware
+make image PROFILE="$PROFILE" PACKAGES="$PACKAGES $EXCLUDED" FILES="$FILES"
