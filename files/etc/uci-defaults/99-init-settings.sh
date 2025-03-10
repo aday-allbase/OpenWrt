8a5160a51920af613631b3a20a6ae43f963f853d
aday-allbase <maxroepz48@gmail.com>
2025-03-11 01:44:41 +0700

    aday

---
+++ files/etc/uci-defaults/99-init-settings.sh
@@ -0,0 +1,162 @@
+#!/bin/sh
+
+# Create a log file with timestamp
+LOGFILE="/root/setup_$(date +%Y%m%d_%H%M%S).log"
+exec > "$LOGFILE" 2>&1
+
+# Function for logging with timestamps
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+# System information banner
+log "==================== SYSTEM INFORMATION ===================="
+log "Installed Time: $(date '+%A, %d %B %Y %T')"
+log "Processor: $(ubus call system board | grep '\"system\"' | sed 's/ \+/ /g' | awk -F'\"' '{print $4}')"
+log "Device Model: $(ubus call system board | grep '\"model\"' | sed 's/ \+/ /g' | awk -F'\"' '{print $4}')"
+log "Device Board: $(ubus call system board | grep '\"board_name\"' | sed 's/ \+/ /g' | awk -F'\"' '{print $4}')"
+log "Memory: $(free -m | grep Mem | awk '{print $2}') MB"
+log "Storage: $(df -h / | tail -1 | awk '{print $2}')"
+log "==================== CONFIGURATION START ===================="
+
+# Firmware customization
+log "Customizing firmware information..."
+sed -i -E "s|icons/port_%s.png|icons/port_%s.gif|g" /www/luci-static/resources/view/status/include/29_ports.js
+
+# System user configuration
+log "Setting up root password..."
+(echo "root"; sleep 1; echo "root") | passwd > /dev/null
+
+# Time zone and NTP configuration
+log "Setting up time zone to Asia/Jakarta and NTP servers..."
+uci set system.@system[0].hostname='OPEN-WRT'
+uci set system.@system[0].timezone='WIB-7'
+uci set system.@system[0].zonename='Asia/Jakarta'
+uci -q delete system.ntp.server
+uci add_list system.ntp.server="0.pool.ntp.org"
+uci add_list system.ntp.server="1.pool.ntp.org"
+uci add_list system.ntp.server="id.pool.ntp.org"
+uci add_list system.ntp.server="time.google.com"
+uci add_list system.ntp.server="time.cloudflare.com"
+uci commit system
+
+# Network interface configuration
+log "Configuring network interfaces..."
+# LAN configuration
+uci set network.lan.ipaddr="192.168.1.1"
+uci set network.lan.netmask="255.255.255.0"
+uci set network.lan.dns="8.8.8.8,1.1.1.1"
+
+# WAN configuration
+uci set network.wan=interface 
+uci set network.wan.proto='modemmanager'
+uci set network.wan.device='/sys/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb2/2-1'
+uci set network.wan.apn='internet'
+uci set network.wan.auth='none'
+uci set network.wan.iptype='ipv4'
+
+# Add failover WAN interface
+log "Adding failover WAN interface..."
+uci set network.wan2=interface
+uci set network.wan2.proto='dhcp'
+uci set network.wan2.device='eth1'
+uci commit network
+
+# Firewall configuration
+log "Configuring firewall..."
+uci set firewall.@zone[1].network='wan wan2'
+uci commit firewall
+
+# Disable IPv6
+log "Disabling IPv6..."
+uci -q delete dhcp.lan.dhcpv6
+uci -q delete dhcp.lan.ra
+uci -q delete dhcp.lan.ndp
+uci commit dhcp
+
+# UI configuration
+log "Setting up UI configuration..."
+# Set material as default theme
+uci set luci.main.mediaurlbase='/luci-static/material' && uci commit
+
+# Configure TTYD
+log "Configuring TTYD..."
+uci set ttyd.@ttyd[0].command='/bin/bash --login'
+uci set ttyd.@ttyd[0].interface='@lan'
+uci set ttyd.@ttyd[0].port='7681'
+uci commit ttyd
+
+# USB modem configuration - remove problematic USB mode switch entries
+log "Configuring USB modem settings..."
+# Function to safely edit USB mode switch configuration
+edit_usb_mode_json() {
+  local vid_pid=$1
+  log "Removing USB mode switch for $vid_pid"
+  sed -i -e "/$vid_pid/,+5d" /etc/usb-mode.json
+}
+
+# Remove specific USB mode switches
+edit_usb_mode_json "12d1:15c1" # Huawei ME909s
+edit_usb_mode_json "413c:81d7" # DW5821e
+edit_usb_mode_json "1e2d:00b3" # Thales MV31-W T99W175
+
+# Disable XMM modem service
+log "Disabling XMM modem service..."
+uci set xmm-modem.@xmm-modem[0].enable='0'
+uci commit xmm-modem
+
+# Setup utility scripts
+log "Setting up utility scripts..."
+for script in /sbin/free.sh; do
+  if [ -f "$script" ]; then
+    chmod +x "$script"
+    log "Made $script executable"
+  fi
+done
+
+chmod +x /usr/bin/aturttl
+chmod +x /usr/bin/expand_rootfs
+chmod +x /usr/bin/patchoc.sh
+chmod +x /usr/bin/speedtest
+
+# Setup PHP for web applications
+log "Setting up PHP..."
+uci set uhttpd.main.ubus_prefix='/ubus'
+uci set uhttpd.main.interpreter='.php=/usr/bin/php-cgi'
+uci set uhttpd.main.index_page='cgi-bin/luci'
+uci add_list uhttpd.main.index_page='index.html'
+uci add_list uhttpd.main.index_page='index.php'
+uci commit uhttpd
+
+# Optimize PHP configuration
+if [ -f "/etc/php.ini" ]; then
+  sed -i -E "s|memory_limit = [0-9]+M|memory_limit = 128M|g" /etc/php.ini
+  sed -i -E "s|max_execution_time = [0-9]+|max_execution_time = 60|g" /etc/php.ini
+  sed -i -E "s|display_errors = On|display_errors = Off|g" /etc/php.ini
+  sed -i -E "s|;date.timezone =|date.timezone = Asia/Jakarta|g" /etc/php.ini
+  log "PHP configuration optimized"
+fi
+
+# Create symbolic links for PHP
+ln -sf /usr/bin/php-cli /usr/bin/php
+[ -d /usr/lib/php8 ] && [ ! -d /usr/lib/php ] && ln -sf /usr/lib/php8 /usr/lib/php
+/etc/init.d/uhttpd restart
+
+# Setup TinyFM file manager
+log "Setting up TinyFM file manager..."
+mkdir -p /www/tinyfm
+ln -sf / /www/tinyfm/rootfs
+
+# Complete setup
+log "==================== CONFIGURATION COMPLETE ===================="
+log "All setup tasks completed successfully!"
+log "Cleaning up and finalizing..."
+
+# Clean up the setup script
+rm -f /etc/uci-defaults/$(basename $0) 2>/dev/null
+
+echo "Setup complete! The system will now reboot in 5 seconds..."
+sleep 5
+reboot
+
+exit 0
--- package-openwrt/build-armsr.sh
+++ package-openwrt/build-armsr.sh
@@ -7,23 +7,32 @@ make info
 PROFILE="generic"
 
 # Define packages
-PACKAGES="luci-compat luci-lua-runtime dnsmasq dnsmasq-full cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua \
-liblucihttp liblucihttp-lua libubus-lua lua luci luci-app-firewall luci-app-opkg \
-luci-base luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
-luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp luci-ssl \
-luci-theme-bootstrap px5g-wolfssl rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci \
-rpcd-mod-rrdns uhttpd uhttpd-mod-ubus usbutils htop \
-kmod-usb-net kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-acm kmod-usb-net-qmi-wwan \
-kmod-usb-net-rndis kmod-usb-serial-qualcomm kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial \
-kmod-nls-utf8 kmod-usb-serial-option kmod-usb-serial-sierrawireless kmod-usb-uhci kmod-usb2 \
-kmod-usb-net-ipheth kmod-usb-net-cdc-mbim usbmuxd libusbmuxd-utils libimobiledevice-utils \
-usb-modeswitch nano picocom minicom wget \
-ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash kmod-fs-btrfs kmod-mac80211 \
-kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
-kmod-fs-ext4 kmod-fs-vfat kmod-fs-xfs kmod-fs-btrfs kmod-fs-f2fs kmod-fs-squashfs \
-kmod-fs-ntfs kmod-fs-ntfs3 kmod-usb-storage kmod-usb-storage-extras \
-kmod-nft-fib kmod-nft-bridge kmod-nft-nat \
-blockd block-mount e2fsprogs fdisk parted blkid kmod-fs-cifs kmod-fs-exfat ntfs-3g ntfs-3g-utils gdisk tune2fs resize2fs losetup cfdisk f2fs-tools kmod-usb-storage-uas kmod-usb-ledtrig-usbport"
+PACKAGES="-procd-ujail -dnsmasq dnsmasq-full cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua \
+luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
+cpusage ttyd dmesg luci-lib-ipkg git git-http \
+zram-swap adb parted losetup resize2fs luci-ssl block-mount htop wget-ssl \
+tar unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server \
+liblucihttp liblucihttp-lua libubus-lua lua luci-app-firewall luci-app-opkg \
+ca-bundle coreutils-sleep fontconfig coreutils-whoami file lolcat \
+luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp \
+luci-theme-material rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci \
+rpcd-mod-rrdns uhttpd uhttpd-mod-ubus coreutils coreutils-base64 coreutils-stty libc coreutils-stat \
+libuci-lua iptables iptables-legacy \
+iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat luci-lua-runtime zoneinfo-asia zoneinfo-core \
+perl perlbase-base perlbase-bytes perlbase-class perlbase-config perlbase-cwd perlbase-dynaloader perlbase-errno perlbase-essential perlbase-fcntl perlbase-file \
+perlbase-filehandle perlbase-i18n perlbase-integer perlbase-io perlbase-list perlbase-locale perlbase-params perlbase-posix \
+perlbase-re perlbase-scalar perlbase-selectsaver perlbase-socket perlbase-symbol perlbase-tie perlbase-time perlbase-unicore perlbase-utf8 perlbase-xsloader \
+coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base \
+kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179 \
+kmod-mii kmod-usb-net kmod-usb-wdm kmod-usb-net-qmi-wwan uqmi luci-proto-qmi \
+kmod-usb-net-cdc-ether kmod-usb-serial-option kmod-usb-serial kmod-usb-serial-wwan qmi-utils \
+kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim umbim \
+modemmanager  modemmanager-rpcd luci-proto-modemmanager libmbim libqmi usbutils luci-proto-mbim luci-proto-ncm \
+kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial-sierrawireless \
+kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-utils libimobiledevice-utils usb-modeswitch kmod-nls-utf8 mbim-utils xmm-modem \
+kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3 libusb-1.0-0 kmod-usb3 kmod-r8169 kmod-lan743x \
+luci-app-diskman luci-app-disks-info smartmontools kmod-usb-storage kmod-usb-storage-uas ntfs-3g \
+php8 php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring"
 
 # Files directory for custom configurations or files
 FILES="files"
